8554b694c2295984a233c638c99d81286e2a2174
muddydixon <muddydixon@gmail.com>
2013-09-06 12:07:39 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Data::Cube - It's new $module
+
+# SYNOPSIS
+
+    use Data::Cube;
+
+# DESCRIPTION
+
+Data::Cube is ...
+
+# LICENSE
+
+Copyright (C) muddydixon.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+muddydixon <muddydixon@gmail.com>
---
+++ lib/Data/Cube.pm
@@ -0,0 +1,39 @@
+package Data::Cube;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Data::Cube - It's new $module
+
+=head1 SYNOPSIS
+
+    use Data::Cube;
+
+=head1 DESCRIPTION
+
+Data::Cube is ...
+
+=head1 LICENSE
+
+Copyright (C) muddydixon.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+muddydixon E<lt>muddydixon@gmail.comE<gt>
+
+=cut
+
